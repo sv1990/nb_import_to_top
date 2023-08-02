a578c68ad6bad49ac710e2b7e688753f2b49fad1
Semir Vrana <semir.vrana@gmx.de>
2023-08-02 19:17:47 +0200

    Initial packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="nbisort",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=["isort >= 5", "more-itertools", "nbformat"],
+    entry_points={"console_scripts": ["nbisort=nbisort.__main__:main"]},
+)
