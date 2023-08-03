1f35e97ffb3363e3387be037738d6cb1eebff6b7
Semir Vrana <semir.vrana@gmx.de>
2023-08-03 22:23:16 +0200

    Walk folders recursively

--- nbisort/__main__.py
+++ nbisort/__main__.py
@@ -9,21 +9,37 @@ import nbformat
 from nbisort import move_imports_to_top
 
 
-def main() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("notebook", type=Path)
+def format_notebook(notebook: Path) -> None:
+    if notebook.suffix != ".ipynb":
+        return
 
-    args = parser.parse_args()
-
-    with open(args.notebook) as f:
+    with open(notebook) as f:
         nb = nbformat.read(f, as_version=4)
 
-    copyfile(args.notebook, args.notebook.with_suffix(f"{args.notebook.suffix}.bak"))
+    copyfile(notebook, notebook.with_suffix(f"{notebook.suffix}.bak"))
 
     nb = move_imports_to_top(nb)
 
-    with open(args.notebook, "w") as f:
+    with open(notebook, "w") as f:
         nbformat.write(nb, f)
+
+
+def format_path(path: Path) -> None:
+    if path.is_file():
+        format_notebook(path)
+    else:
+        for subpath in path.iterdir():
+            format_path(subpath)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("notebook", type=Path, nargs="?")
+
+    args = parser.parse_args()
+
+    path: Path = args.notebook or Path.cwd()
+    format_path(path)
 
 
 if __name__ == "__main__":
