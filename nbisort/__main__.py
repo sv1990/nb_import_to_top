caa4e7043fb0c3a96a3eb2bbe6c03d876266f865
Semir Vrana <semir.vrana@gmx.de>
2023-09-04 08:49:25 +0200

    Add option verbose

--- nbisort/__main__.py
+++ nbisort/__main__.py
@@ -8,12 +8,15 @@ import nbformat
 from nbisort import nbisort
 
 
-def format_notebook(notebook: Path) -> None:
+def format_notebook(notebook: Path, verbose: bool = False) -> None:
     if notebook.suffix != ".ipynb":
         return
 
     with open(notebook) as f:
         nb = nbformat.read(f, as_version=4)
+
+    if verbose:
+        print(f"Formatting {notebook}...")
 
     nb = nbisort(nb)
 
@@ -21,22 +24,23 @@ def format_notebook(notebook: Path) -> None:
         nbformat.write(nb, f)
 
 
-def format_path(path: Path) -> None:
+def format_path(path: Path, verbose: bool = False) -> None:
     if path.is_file():
-        format_notebook(path)
+        format_notebook(path, verbose=verbose)
     else:
         for subpath in path.iterdir():
-            format_path(subpath)
+            format_path(subpath, verbose=verbose)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("notebook", type=Path, nargs="?")
+    parser.add_argument("--verbose", "-v", action="store_true")
 
     args = parser.parse_args()
 
     path: Path = args.notebook or Path.cwd()
-    format_path(path)
+    format_path(path, verbose=args.verbose)
 
 
 if __name__ == "__main__":
