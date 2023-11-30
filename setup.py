ffbccf75b5afb3de366101a37d183c5e3524920e
Semir Vrana <semir.vrana@gmx.de>
2023-11-30 20:38:11 +0100

    Use poetry

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="nbisort",
-    version="0.0.1",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=["isort >= 5", "more-itertools", "nbformat"],
-    entry_points={"console_scripts": ["nbisort=nbisort.__main__:main"]},
-)
